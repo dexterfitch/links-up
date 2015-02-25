22d605cff4789d1c77f34052de4ce326eb363f69
Dexter Fitch & tucker mayo <student@epicodus.com>
2015-02-25 14:44:42 -0800

    Add rating logic.

--- controllers/LinksController.js
+++ controllers/LinksController.js
@@ -2,8 +2,19 @@ linkMachine.controller('LinksCtrl', function LinksCtrl($scope, $stateParams, Cat
   $scope.category = UtilitiesFactory.findById(CategoriesFactory.categories, $stateParams.categoryId)
 
   $scope.addLink = function() {
-    $scope.category.links.push({ name: $scope.linkName, website: $scope.webSite });
+    $scope.category.links.push({ name: $scope.linkName, website: $scope.webSite, rating: 0 });
     $scope.linkName = null;
     $scope.webSite = null;
   };
+
+  $scope.addToRating = function(link) {
+    var index = factory.links.indexOf(link);
+    factory.links[index].rating += 1;
+  };
+
+  $scope.subtractFromRating = function(link) {
+    var index = factory.links.indexOf(link);
+    factory.links[index].rating -= 1;
+  };
+
 });
